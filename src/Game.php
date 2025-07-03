766660349834d954127c9fc8b2a2a9d9d92913a6
Darío Candotti <83561398+kamishimoemon@users.noreply.github.com>
2025-07-03 20:43:19 -0300

    Rename draw listener to gameOver

--- src/Game.php
+++ src/Game.php
@@ -6,25 +6,42 @@ namespace TicTacToe;
 
 class Game
 {
-	private array $spaces = [];
-	private ?Mark $currentMark = null;
+        private array $spaces = [];
+        private ?Mark $currentMark = null;
+        private array $listeners = [];
 
-	public function place (Mark $mark, Position $position): void
-	{
-		if ($this->currentMark !== null) {
-			$this->currentMark->validateTurn($mark);
-		}
+        public function place (Mark $mark, Position $position): void
+        {
+                if ($this->currentMark !== null) {
+                        $this->currentMark->validateTurn($mark);
+                }
 
 		if (isset($this->spaces[$position->value])) {
 			throw new InvalidMove();
 		}
 
-		$this->spaces[$position->value] = $mark;
-		$this->currentMark = $mark;
-	}
+                $this->spaces[$position->value] = $mark;
+                $this->currentMark = $mark;
 
-	public static function new (): Game
-	{
-		return new Game();
-	}
+                if (count($this->spaces) === count(Position::cases())) {
+                        $this->gameOver();
+                }
+        }
+
+        public function attach (GameListener $listener): void
+        {
+                $this->listeners[] = $listener;
+        }
+
+        private function gameOver (): void
+        {
+                foreach ($this->listeners as $listener) {
+                        $listener->gameOver($this);
+                }
+        }
+
+        public static function new (): Game
+        {
+                return new Game();
+        }
 }
---
+++ test/GameOverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use TicTacToe\Game;
+use TicTacToe\GameListener;
+use TicTacToe\Mark;
+use TicTacToe\Position;
+
+class GameOverTest extends TestCase
+{
+    #[Test]
+    function notifiesDrawWhenNoMovesAvailable(): void
+    {
+        $game = Game::new();
+
+        $listener = $this->createMock(GameListener::class);
+        $listener->expects($this->once())->method('gameOver')->with($this->identicalTo($game));
+        $game->attach($listener);
+
+        $mark = Mark::X;
+        foreach (Position::cases() as $position) {
+            $game->place($mark, $position);
+            $mark = $mark->not();
+        }
+    }
+}
